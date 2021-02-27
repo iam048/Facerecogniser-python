5a221f54bd40f436b6a88bfbe1d4d9501be849e8
iam048 <36331279+iam048@users.noreply.github.com>
2021-02-27 22:08:37 +0530

    Intial Update

---
+++ README.md
@@ -0,0 +1,18 @@
+# Python Face Detection & recognition Module (Personal)
+
+> A Python Script that Detect faces form target image and compare against known faces and extract the unknown match 
+
+## Usage: 
+
+```bash
+python face_rec.py [-h] [--extract-unknown] --target-dir TARGETDIR [--gen-has] [--known-has DAT]
+
+optional arguments:
+  -h, --help            	show this help message and exit
+  --extract-unknown     	Extract the Unknown faces to the Local disk
+  --target-dir TARGETDIR 	Target Directory to Compare Faces / Generate HAS
+  --gen-has             	Generates file of known faces
+  --known-has DAT       	Holds a Known faces
+```
+
+***More Documentary will be Updated Soon***
--- face_rec.py
+++ face_rec.py
@@ -19,11 +19,6 @@ parser.add_argument("--target-dir",
 	help="Target Directory to Compare Faces / Generate HAS",
 	required=True
 	)
-parser.add_argument("--draw",
-	dest="DRAW",
-	help="Draws a Rectangle Along Known Faces",
-	action="store_true"
-	)
 parser.add_argument("--gen-has",
 	dest="GEN",
 	help="Generates file of known faces",
@@ -40,75 +35,78 @@ TARGET = ARG.targetDir
 TOL = 0.5
 MODEL = "hog"
 
-def extract_faces(image):
-	encodings = []
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	faces = face_recognition.face_locations(image, model=MODEL)
-
-	if faces is not None:
-		for (top, right, bottom, left) in faces:
-			face = image[top:bottom, left:right]
-
-
-			face_enc = face_recognition.face_encodings(face)
-			result = compare(face_enc)
-			if result is not True:
-				cv2.imwrite(f"{UNKNOWN}/{random.random()}.jpg", face)
-
-def compare(encodings):
-	print("Processing Unknown faces")
-	data = open(ARG.DAT, 'rb')
-	data = pickle.load(data)
-
-	names = []
-	faces = []
+class faceRecogniser():
 	
-	for key in data:
-		names.append(key)
-		faces.append(data[key])
+	def extract_faces(self,image):
+		encodings = []
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		faces = face_recognition.face_locations(image, model=MODEL)
 
-	for face_encoding in encodings:
-		match = face_recognition.compare_faces(faces, face_encoding, TOL)
-		try:
-			if True in match:
-				print(f"Ignoring Known Match: {names[match.index(True)]}")
-				return True
-			else:
-				return False
-		except IndexError:
-			pass
+		if faces is not None:
+			print("Processing Unknown faces")
+			for (top, right, bottom, left) in faces:
+				face = image[top:bottom, left:right]
 
-def gen_known_has():
-	print("Generating Data")
-	faces = []
-	names = []
-	# list all files in directory
-	for name in os.listdir(TARGET):
-		for filename in os.listdir(f"{TARGET}/{name}"):
-			image = face_recognition.load_image_file(f"{TARGET}/{name}/{filename}")
-			encoding = face_recognition.face_encodings(image)[0]
-			
-			faces.append(encoding)
-			names.append(name)
+				face_enc = face_recognition.face_encodings(face)
+				result = self.compare(face_enc)
+				if result is not True:
+					cv2.imwrite(f"{UNKNOWN}/{random.random()}.jpg", face)
 
-			has = {}
-			for enc, name in zip(faces, names):
-				has[name] = enc
+	def compare(self,encodings) -> bool:
+		data = open(ARG.DAT, 'rb')
+		data = pickle.load(data)
 
-			f = open("known_faces_hasscasdes.dat", 'wb')
-			pickle.dump(has, f)
-			f.close()
+		names = []
+		faces = []
+		
+		for key in data:
+			names.append(key)
+			faces.append(data[key])
+
+		for face_encoding in encodings:
+			match = face_recognition.compare_faces(faces, face_encoding, TOL)
+			try:
+				if True in match:
+					name = names[match.index(True)]
+					print(f"Ignoring Known Match: {name}")
+					return True
+				else:
+					return False
+			except IndexError:
+				pass
+
+	def gen_known_has(self):
+		print("[+] Generating Data")
+		faces = []
+		names = []
+		# list all files in directory
+		for name in os.listdir(TARGET):
+			for filename in os.listdir(f"{TARGET}/{name}"):
+				image = face_recognition.load_image_file(f"{TARGET}/{name}/{filename}")
+				encoding = face_recognition.face_encodings(image)[0]
+				
+				faces.append(encoding)
+				names.append(name)
+
+				has = {}
+				for enc, name in zip(faces, names):
+					has[name] = enc
+
+				f = open("known_faces_hasscasdes.dat", 'wb')
+				pickle.dump(has, f)
+				f.close()
 
 #Detecting Faces Unknown
 try:
+	recogniser = faceRecogniser()
 	if ARG.EXTRACT:
 		for filename in os.listdir(TARGET):
 			print(filename)
 			image = face_recognition.load_image_file(f"{TARGET}/{filename}")
-			extract_faces(image)
+			recogniser.extract_faces(image)
 
 	if ARG.GEN:
-		gen_known_has()
+		recogniser.gen_known_has()
 
 except KeyboardInterrupt:
 	print("Bye")
